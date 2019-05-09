2109913b03164838fd484f8e2d212128bd75ccb4
Rok Lampret <rl7811@student.uni-lj.si>
2019-05-09 23:15:42 +0200

    Test every 100 score marker to stop exploration

--- qlearn.py
+++ qlearn.py
@@ -1,11 +1,18 @@
 import argparse
 import os
 import random
+import sys
 from datetime import datetime
 
 import numpy as np
 
 from game import Game
+
+
+def xprint(*args, **kwargs):
+    print(*args, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
+
 
 parser = argparse.ArgumentParser(description='Q-Learning for Chrome Dino run.',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -32,6 +39,8 @@ try:
         q_table = np.zeros(
             [env.observation_space_n, env.action_space_n], dtype=np.float16)
 
+    score_limit = 0
+
     # Hyperparameters
     alpha = 0.2
     gamma = 0.5
@@ -47,7 +56,8 @@ try:
             # Cant jump while jumping
             if state % 2 == 1:
                 action = 0
-            elif random.uniform(0, 1) < epsilon:
+            elif (random.uniform(0, 1) < epsilon and
+                  env.get_score() >= score_limit):
                 action = random.randint(0, 1)  # Explore action space
                 if args.verbose and action:
                     print('Random jump')
@@ -68,14 +78,36 @@ try:
 
             state = next_state
 
-        print(f"{datetime.now()} Episode: {i}")
+        xprint(f"{datetime.now()} Episode: {i}")
+        if i % 200 == 0:
+            # Evercleay 200 episodes test if it can pass the 100 score section
+            # 10 times in a row
+            test_passed = True
+            for testn in range(10):
+                state = env.reset()
+                game_over = False
+
+                while not game_over:
+                    # Exploit learned values
+                    action = np.argmax(q_table[state])
+                    state, _, game_over = env.take_action(action)
+
+                if env.get_score() < score_limit + 100:
+                    xprint(f'Test {testn} failed.')
+                    test_passed = False
+                    break
+
+            if test_passed:
+                score_limit = score_limit + 100
+                xprint(f'Test {score_limit} passed')
+
         if i % 500 == 0:
             # old_file = f'q_table_{i-50}.npy'
             # if os.path.isfile(old_file):
             #     os.remove(old_file)
             np.save(f'q_table_{i}.npy', q_table)
 
-    print("Training finished.")
+    xprint("Training finished.")
 
 
 finally:
