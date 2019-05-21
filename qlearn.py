b05451c9f630814c1ed66cd4abb76982ebde80b8
Rok Lampret <rl7811@student.uni-lj.si>
2019-05-21 23:04:18 +0200

    Dont print so much, remove test

--- qlearn.py
+++ qlearn.py
@@ -9,25 +9,17 @@ import numpy as np
 from game import Game
 
 
-def xprint(*args, **kwargs):
-    print(*args, **kwargs)
-    print(*args, file=sys.stderr, **kwargs)
-
-
 parser = argparse.ArgumentParser(description='Q-Learning for Chrome Dino run.',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+parser.add_argument("--score", type=int, default=0,
+                    help="Starting score limit.")
 parser.add_argument("--iter", type=int, default=0,
                     help="Starting iteration number.")
 parser.add_argument("--qtable", type=str, default=None,
                     help="Path to existing Q-table.")
-parser.add_argument("--silent", dest='verbose', action='store_false',
-                    help="Do not print states and rewards.")
 
 args = parser.parse_args()
-
-
-env = Game()
 
 try:
     i_starting = 1
@@ -37,9 +29,11 @@ try:
             i_starting = args.iter + 1
     else:
         q_table = np.zeros(
-            [env.observation_space_n, env.action_space_n], dtype=np.float16)
+            [Game.observation_space_n, Game.action_space_n], dtype=np.float16)
 
-    score_limit = 0
+    score_limit = args.score
+
+    env = Game()
 
     # Hyperparameters
     alpha = 0.2
@@ -56,17 +50,13 @@ try:
             # Cant jump while jumping
             if state % 2 == 1:
                 action = 0
-            elif (random.uniform(0, 1) < epsilon and
-                  env.get_score() >= score_limit):
+            elif (env.get_score() >= score_limit and
+                  random.uniform(0, 1) < epsilon):
                 action = random.randint(0, 1)  # Explore action space
-                if args.verbose and action:
-                    print('Random jump')
             else:
                 action = np.argmax(q_table[state])  # Exploit learned values
 
             next_state, reward, game_over = env.take_action(action)
-            if args.verbose:
-                print(f'{next_state}\t{reward}')
 
             old_value = q_table[state, action]
             next_max = np.max(q_table[next_state])
@@ -78,36 +68,18 @@ try:
 
             state = next_state
 
-        xprint(f"{datetime.now()} Episode: {i}")
-        if i % 200 == 0:
-            # Evercleay 200 episodes test if it can pass the 100 score section
-            # 10 times in a row
-            test_passed = True
-            for testn in range(10):
-                state = env.reset()
-                game_over = False
+        # Failed at lower score, lower score_limit
+        curr_score = env.get_score()
+        print(f"{datetime.now()} {i} {score_limit} {curr_score}")
+        score_limit = (curr_score // 100) * 100
 
-                while not game_over:
-                    # Exploit learned values
-                    action = np.argmax(q_table[state])
-                    state, _, game_over = env.take_action(action)
-
-                if env.get_score() < score_limit + 100:
-                    xprint(f'Test {testn} failed.')
-                    test_passed = False
-                    break
-
-            if test_passed:
-                score_limit = score_limit + 100
-                xprint(f'Test {score_limit} passed')
+        if i % 20 == 0:
+            score_limit += 100
 
         if i % 500 == 0:
-            # old_file = f'q_table_{i-50}.npy'
-            # if os.path.isfile(old_file):
-            #     os.remove(old_file)
             np.save(f'q_table_{i}.npy', q_table)
 
-    xprint("Training finished.")
+    print("Training finished.")
 
 
 finally:
